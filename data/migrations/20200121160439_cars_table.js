41cd58d42f68d1f3d6b1ad072a4d6050e14b0dba
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-21 16:29:03 -0500

    filled out migrations file

--- data/migrations/20200121160439_cars_table.js
+++ data/migrations/20200121160439_cars_table.js
@@ -1,6 +1,21 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable("songs", tbl => {
+        // id column, integer, primary key, auto-increment
+        tbl.increments();
+
+        tbl.integer("VIN");
+
+        tbl.string("make", 255).index();
+
+        tbl.string("model",255).index();
+
+        tbl.integer("mileage");
+        
+
+        // adds created_at and updated_at columns
+        //tbl.timestamps(true, true);
+    });
 };
 
 exports.down = function(knex) {
