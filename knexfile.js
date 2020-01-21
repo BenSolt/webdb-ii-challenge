0f900ffcb768db51f9b89c9f8ebdec09f38d6a14
BenSolt <52627840+BenSolt@users.noreply.github.com>
2020-01-21 15:36:42 -0500

    created several files

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+module.exports = {
+    development: {
+      
+        client: "sqlite3", // the db driver
+       
+        connection: {
+            filename: "./data/car-dealer.db3",
+        },
+        useNullAsDefault: true, // ONLY needed for SQLite
+        migrations: {
+            directory: "./data/migrations",
+        },
+    },
+    staging: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password",
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+    },
+    production: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password",
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+    },
+};
+// npm i -g knex or just use npx knex for commands
+// run: knex init
+// copy from fruits router the configuration object to knexfile.js
+// add the migrations key to knexfile.js
+// run: knex migrate:make songs_table
+//
